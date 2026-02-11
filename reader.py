f38687a9ae5b7cf11e12fa859a01bcafc164a0e9
אלעזר גרשי <emg4706@gmail.com>
2026-02-11 16:56:11 +0200

    fixed load file

--- reader.py
+++ reader.py
@@ -3,6 +3,6 @@
 # load file
 def load_file(path):
     with open(path,"r",encoding="utf-8") as f:
-        lists = [lst.splitlines() for lst in f]
+        lists = [lst.strip().split(",") for lst in f]
         return lists
 
