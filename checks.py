fa2bb64248596ec9facc9730418d3dd9b2fa0666
אלעזר גרשי <emg4706@gmail.com>
2026-02-11 18:58:29 +0200

    added get_suspicious_lines

--- checks.py
+++ checks.py
@@ -1,7 +1,12 @@
 from reader import load_file
+from config import sensitive_ports
 
 # check_external_ip
 def external_ip(f): # 'f' = file with list of lists
     ips = [lst[1] for lst in f if not lst[1].startswith("192.168") and not lst[1].startswith("10.")]
     return ips
+
+def get_suspicious_lines(f):
+    suspicious_lines = [line for line in f if line[3] in sensitive_ports]
+    return suspicious_lines
 
--- reader.py
+++ reader.py
@@ -5,4 +5,3 @@ def load_file(path):
     with open(path,"r",encoding="utf-8") as f:
         lists = [lst.strip().split(",") for lst in f]
         return lists
-
