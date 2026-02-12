34fe91b3f2b9d2660fadb2779a82bde44f91c8d4
אלעזר גרשי <emg4706@gmail.com>
2026-02-12 15:22:19 +0200

    added protocol by port

--- analyzer.py
+++ analyzer.py
@@ -5,3 +5,8 @@ def count_requests(data):
     ips = [line[1] for line in data]
     list_requests = {ip: ips.count(ip) for ip in set(ips) }
     return list_requests
+
+def protokol_by_port(data):
+    all_ports = [[line[3],line[4]] for line in data]
+    dict_port = { port[0]: port[1] for port in all_ports }
+    return dict_port
