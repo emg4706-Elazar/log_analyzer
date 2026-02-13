5597084331299f966e9b96c89e41c28e2759ba49
אלעזר גרשי <emg4706@gmail.com>
2026-02-13 16:13:11 +0200

    added flag_suspicious_ips

--- analyzer.py
+++ analyzer.py
@@ -1,4 +1,5 @@
-
+from reader import load_file
+from config import sensitive_ports,large_packet
 
 
 def count_requests(data):
@@ -6,7 +7,28 @@ def count_requests(data):
     list_requests = {ip: ips.count(ip) for ip in set(ips) }
     return list_requests
 
-def protokol_by_port(data):
+def protocol_by_port(data):
     all_ports = [[line[3],line[4]] for line in data]
     dict_port = { port[0]: port[1] for port in all_ports }
     return dict_port
+
+# return dict with list suspicious flags
+def flag_suspicious_ips(data):
+    dicti = {}
+    for line in data:
+        dicti[line[1]] = []
+        # check night activity
+        hour = line[0][11] + line[0][12]
+        if "00" <= hour <"06":
+            dicti[line[1]].append("NIGHT_ACTIVITY")
+        # check external ip
+        if not line[1].startswith("192.168") and not line[1].startswith("10."):
+            dicti[line[1]].append("EXTERNAL_IP")
+        # check sensitive_port
+        if line[3] in sensitive_ports:
+            dicti[line[1]].append("SENSITIVE_PORT")
+        if int(line[-1]) > large_packet:
+            dicti[line[1]].append("LARGE_PACKET")
+        if not dicti[line[1]]:
+            dicti[line[1]].append(None)
+    return dicti
--- checks.py
+++ checks.py
@@ -7,17 +7,22 @@ def external_ip(data): # 'f' = file with list of lists
     ips = [lst[1] for lst in data if not lst[1].startswith("192.168") and not lst[1].startswith("10.")]
     return ips
 
-def get_suspicious_lines(data):
+def get_suspicious_ports(data):
     suspicious_lines = list(filter(lambda line: line[3] in sensitive_ports,data))
     return suspicious_lines
 
-def get_lines_by_size(data):
+def get_packets_by_size(data):
     large_packets = list(filter(lambda line: int(line[-1])>large_packet, data))
     return large_packets
 
 def get_traffic_labeling(data):
     labeled_packets =  [ line + ["LARGE"] if int(line[-1]) > large_packet else line + ["NORMAL"] for line in data]
     return labeled_packets
+
+def night_activity(data):
+    get_time = lambda n: n[0].strip().split()
+    night_packets = filter(lambda line: "00"<= (get_time(line)[1].split(":")[0])<"06" , data)
+    return list(night_packets)
 
 
 
