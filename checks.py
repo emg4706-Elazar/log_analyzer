1d8dd838711c19860957b8adf3f80a3de042f284
אלעזר גרשי <emg4706@gmail.com>
2026-02-16 22:03:52 +0200

    added filter_suspicious_ips

--- checks.py
+++ checks.py
@@ -3,15 +3,15 @@ from config import sensitive_ports,large_packet
 from analyzer import count_requests
 
 # check_external_ip
-def external_ip(data): # 'f' = file with list of lists
+def get_external_ip(data): # 'f' = file with list of lists
     ips = [lst[1] for lst in data if not lst[1].startswith("192.168") and not lst[1].startswith("10.")]
     return ips
 
-def get_suspicious_ports(data):
+def get_sensitive_port_traffic(data):
     suspicious_lines = list(filter(lambda line: line[3] in sensitive_ports,data))
     return suspicious_lines
 
-def get_packets_by_size(data):
+def get_large_packets(data):
     large_packets = list(filter(lambda line: int(line[-1])>large_packet, data))
     return large_packets
 
